9413cc7542be32587a025b797755f63f8da49432
Rhys <rhyswoody@gmail.com>
2025-05-06 18:42:15 -0700

    add og image

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export const metadata = {
     url: "https://racein30.com",
     images: [
       {
-        url: "/og-image.jpg",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "Race in 30 - F1 Race Voting Platform",
@@ -36,7 +36,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "VOTE 30: Spoiler free F1 race poll",
     description: "Community-driven F1 race viewing recommendations",
-    images: ["/twitter-image.jpg"],
+    images: ["/og-image.png"],
   },
   robots: {
     index: true,
